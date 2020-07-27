3884828e04b1b7b5ca29779468ed6dc6e48d16ed
Alisa Popova <alisa.popova@gmail.com>
2020-07-27 15:37:07 +0300

    change js files code styles according to eslint error messages - part-2

--- js/app.js
+++ js/app.js
@@ -1,62 +1,61 @@
 import settings from './settings.js';
-import Github from './github.js'
-import UI from './ui.js'
+import Github from './github.js';
+import UI from './ui.js';
 
-
-const githubUserName = document.getElementById("githubUserName");
-const userProfileContainer = document.getElementById("profile");
-const userRepos = document.getElementById("repos");
-const alerts = document.getElementById("alerts");
+const githubUserName = document.getElementById('githubUserName');
+const userProfileContainer = document.getElementById('profile');
+const userRepos = document.getElementById('repos');
+const alerts = document.getElementById('alerts');
 
 const ui = new UI(userProfileContainer, userRepos, alerts);
 const github = new Github(settings);
 
-githubUserName.addEventListener("keyup", onUserKeyUp);
+githubUserName.addEventListener('keyup', onUserKeyUp);
 
 function responseMessageIsOk(message) {
-    if (typeof message !== "undefined" && message.trim().length > 0)  {
-        console.log(`checkHttpResponseMessage: ${message}`);
-        ui.clearProfile();
-        ui.showAlert(message, "danger");
-        return false;
-    }
-    return true;
+  if (!message) {
+    console.log(`checkHttpResponseMessage: ${message}`);
+    ui.clearProfile();
+    ui.showAlert(message, 'danger');
+    return false;
+  }
+  return true;
 }
 
 
 function getUserProfileInfo(username, callback) {
-    github.getUser(username)
-        .then(user => {
-            if (responseMessageIsOk(user.userProfile.message)) {
-                console.log("getUserProfileInfo: ", user);
-                ui.showProfile(user.userProfile);
-                if (typeof callback !== "undefined" && callback != null) {
-                    callback();
-                }
-            }
-        });
+  github.getUser(username)
+      .then((user) => {
+        if (responseMessageIsOk(user.userProfile.message)) {
+          console.log('getUserProfileInfo: ', user);
+          ui.showProfile(user.userProfile);
+          if (!callback) {
+            callback();
+          }
+        }
+      });
 }
 
 
 function getUserReposInfo(username, callback) {
-    github.getUserRepos(username)
-        .then(user => {
-            if (responseMessageIsOk(user.userRepos.message)) {
-                console.log("getUserReposInfo:", user.userRepos);
-                ui.showRepos(user.userRepos);
-                if (typeof callback !== "undefined" && callback != null) {
-                    callback();
-                }
-            }
-        });
+  github.getUserRepos(username)
+      .then((user) => {
+        if (responseMessageIsOk(user.userRepos.message)) {
+          console.log('getUserReposInfo:', user.userRepos);
+          ui.showRepos(user.userRepos);
+          if (!callback) {
+            callback();
+          }
+        }
+      });
 }
 
-function onUserKeyUp (e) {
-    console.log(`onUserKey: e.target=${e.target}`);
+function onUserKeyUp(e) {
+  console.log(`onUserKey: e.target=${e.target}`);
 
-    const username = e.target.value;
+  const username = e.target.value;
 
-    if (username.trim() !== '') {
-        getUserProfileInfo(username, () => getUserReposInfo(username));
-    }
+  if (username.trim() !== '') {
+    getUserProfileInfo(username, () => getUserReposInfo(username));
+  }
 }
--- js/ui.js
+++ js/ui.js
@@ -1,113 +1,114 @@
-import * as format from "./format.js";
+import * as format from './format.js';
 
 export default class UI {
+  constructor(profileContainer, userRepos, alerts) {
+    this.profileContainer = profileContainer;
+    this.userRepos = userRepos;
+    this.alerts = alerts;
+  }
 
-    constructor(profileContainer, userRepos, alerts) {
-        this.profileContainer = profileContainer;
-        this.userRepos = userRepos;
-        this.alerts = alerts;
-    }
+  showProfile(userProfile) {
+    this.profileContainer.innerHTML = `
+        <div class="card card-body">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="${userProfile.avatar_url}" 
+                         alt="avatar" 
+                         class="img-fluid mb-2">
+                    <a href="${userProfile.html_url}" 
+                       target="_blank" 
+                       class="btn btn-primary btn-block">View User Profile</a>
+                </div>
+                <div class="col-md-9">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="badge badge-success">Public Repos: ${userProfile.public_repos}</div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="badge badge-secondary">Following: ${userProfile.following}</div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="badge badge-primary">Followers: ${userProfile.followers}</div>
+                        </div>
+                    </div>                                     
+                    
+                    <div class="text-muted row">
+                        <div class="col-md-3">
+                            <div>Name:</div>
+                            <div>Bio:</div>
+                            <div>Member since:</div>
+                            <div>Email:</div>
+                            <div>Blog/Web-site:</div>
+                            <div>Location:</div>
+                        </div>
+                        <div class="col-md-9">
+                            <div>${format.formatText(userProfile.name)}</div>
+                            <div style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${format.formatText(userProfile.bio)}</div>
+                            <div>${format.formatDate(userProfile.created_at)}</div>
+                            <div>${format.formatText(userProfile.email)}</div>
+                            <div>${format.formatText(userProfile.blog)}</div>
+                            <div> ${format.formatText(userProfile.location)}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+  }
 
-    showProfile(userProfile) {
-        this.profileContainer.innerHTML = `
-                            <div class="card card-body">
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        <img src="${userProfile.avatar_url}" 
-                                             alt="avatar" 
-                                             class="img-fluid mb-2">
-                                        <a href="${userProfile.html_url}" 
-                                           target="_blank" 
-                                           class="btn btn-primary btn-block">View User Profile</a>
-                                    </div>
-                                    <div class="col-md-9">
-                                        <div class="row">
-                                            <div class="col-md-3"><div class="badge badge-success">Public Repos: ${userProfile.public_repos}</div></div>
-                                            <div class="col-md-3"><div class="badge badge-secondary">Following: ${userProfile.following}</div></div>
-                                            <div class="col-md-3"><div class="badge badge-primary">Followers: ${userProfile.followers}</div></div>
-                                        </div>                                     
-                                        
-                                        <div class="text-muted row">
-                                            <div class="col-md-3">
-                                                <div>Name:</div>
-                                                <div>Bio:</div>
-                                                <div>Member since:</div>
-                                                <div>Email:</div>
-                                                <div>Blog/Web-site:</div>
-                                                <div>Location:</div>
-                                            </div>
-                                            <div class="col-md-9">
-                                                <div>${format.formatText(userProfile.name)}</div>
-                                                <div style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${format.formatText(userProfile.bio)}</div>
-                                                <div>${format.formatDate(userProfile.created_at)}</div>
-                                                <div>${format.formatText(userProfile.email)}</div>
-                                                <div>${format.formatText(userProfile.blog)}</div>
-                                                <div> ${format.formatText(userProfile.location)}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`;
+  clearProfile() {
+    this.profileContainer.innerHTML = '';
+    this.alerts.innerHTML = '';
+    this.userRepos.innerHTML = '';
+  }
 
-    }
+  showRepos(repos) {
+    this.userRepos.innerHTML = repos.map((repo) => `<div class="card card-body text-muted">
+                  <div class="row">
+                      <div class="col-md-3">                                        
+                          <a href="${repo.html_url}" 
+                             target="_blank" 
+                             class="btn btn-primary btn-block">${repo.name}</a>
+                      </div>
+                      <div class="col-md-3">                                        
+                          <div>Description:</div>
+                          <div>Language:</div>
+                          <div>Created:</div>
+                          <div>Updated:</div>
+                      </div>
+                      <div class="col-md-6">                                        
+                          <div style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${format.formatText(repo.description)}</div>
+                          <div>${format.formatText(repo.language)}</div>
+                          <div>${format.formatDate(repo.created_at)}</div>
+                          <div>${format.formatDate(repo.updated_at)}</div>
+                      </div>
+                   </div>                    
+              </div>`)
+        .join('');
+  }
 
-    clearProfile() {
-        this.profileContainer.innerHTML = '';
-        this.alerts.innerHTML = '';
-        this.userRepos.innerHTML = '';
-    }
+  showAlert(message, _type) {
+    const type = _type == null ? 'secondary' : _type;
+    const alert = document.createElement('div');
+    const btn = document.createElement('button');
+    const txt = document.createElement('span');
+    alert.classList = `alert alert-dismissible alert-${type}`;
+    btn.classList = 'close';
+    btn.innerHTML = '×';
 
-    showRepos(repos) {
-        this.userRepos.innerHTML = repos.map(repo => `<div class="card card-body text-muted">
-                                <div class="row">
-                                    <div class="col-md-3">                                        
-                                        <a href="${repo.html_url}" 
-                                           target="_blank" 
-                                           class="btn btn-primary btn-block">${repo.name}</a>
-                                    </div>
-                                    <div class="col-md-3">                                        
-                                        <div>Description:</div>
-                                        <div>Language:</div>
-                                        <div>Created:</div>
-                                        <div>Updated:</div>
-                                    </div>
-                                    <div class="col-md-6">                                        
-                                        <div style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${format.formatText(repo.description)}</div>
-                                        <div>${format.formatText(repo.language)}</div>
-                                        <div>${format.formatDate(repo.created_at)}</div>
-                                        <div>${format.formatDate(repo.updated_at)}</div>
-                                    </div>
-                                 </div>                    
-                            </div>
-                            `)
-            .join('');
+    txt.innerHTML = message;
+    alert.appendChild(btn);
+    alert.appendChild(txt);
+    this.clearAlerts();
+    this.alerts.appendChild(alert);
 
-    }
+    const closeLater = setTimeout(() => this.clearAlerts(), 5000);
+    btn.addEventListener('click', () => {
+      this.clearAlerts();
+      clearTimeout(closeLater);
+    });
+  }
 
-    showAlert(message, _type) {
-        let type = typeof _type == "undefined" || _type == null ? 'secondary' : _type;
-        let alert = document.createElement('div');
-        let btn = document.createElement('button');
-        let txt = document.createElement('span');
-        alert.classList = `alert alert-dismissible alert-${type}`;
-        btn.classList = 'close';
-        btn.innerHTML = '×';
-
-        txt.innerHTML = message;
-        alert.appendChild(btn);
-        alert.appendChild(txt);
-        this.clearAlerts();
-        this.alerts.appendChild(alert);
-
-        const closeLater = setTimeout(() => this.clearAlerts(), 5000);
-        btn.addEventListener('click', () => {
-            this.clearAlerts();
-            clearTimeout(closeLater);
-        });
-
-    }
-
-    clearAlerts() {
-        this.alerts.innerHTML = '';
-    }
+  clearAlerts() {
+    this.alerts.innerHTML = '';
+  }
 };
